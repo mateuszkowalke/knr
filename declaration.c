d7413950a2911e3f5ca2df89a2a205c064d5fd49
mateuszkowalke <mateusz.kowalke@gmail.com>
2023-08-14 06:41:20 +0200

    dcl WIP

---
+++ declaration.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAXTOKEN 100
+
+enum { NAME, PARENS, BRACKETS };
+
+void dcl(void);
+void dirdcl(void);
+int gettoken(void);
+int tokentype;
+char token[MAXTOKEN];
+char name[MAXTOKEN];
+char datatype[MAXTOKEN];
+char out[1000];
+
+int main() {
+    while (gettoken() != EOF) {
+        strcpy(datatype, token);
+        out[0] = '\0';
+        dcl();
+        if (tokentype != '\n') {
+            printf("Error: syntax error\n");
+        }
+        printf("%s: %s %s\n", name, out, datatype);
+    }
+    return 0;
+}
+
+void dcl(void) {
+    int ns;
+    for (ns = 0; gettoken() == '*';)
+        ns++;
+    dirdcl();
+    while (ns-- > 0)
+        strcat(out, "pointer to");
+}
+
+void dirdcl(void) {
+    int type;
+    if (tokentype == '(') {
+        dcl();
+        if (tokentype != ')')
+            printf("Error: missing )\n");
+    } else if (tokentype == NAME) {
+        strcpy(name, token);
+    } else {
+        printf("Error: expected NAME or (dcl)\n");
+    }
+    while ((type = gettoken()) == PARENS || type == BRACKETS) {
+        if (type == PARENS) {
+            strcat(out, " function returning");
+        } else {
+            strcat(out, " array");
+            strcat(out, token);
+            strcat(out, " of");
+        }
+   }
+}
+
+int gettoken(void) {
+    int c;
+    char *p = token;
+    while ((c = getchar()) == ' ' || c == '\t');
+    if (c == '(') {
+        if ((c = getchar()) == ')') {
+            strcpy(token, "()");
+            return tokentype = PARENS;
+        } else {
+            ungetc(c, stdin);
+            return tokentype = '(';
+        }
+    }
+}
